1b2cf067592f9428b8179b0cf419069e9d0daf4a
pascual_silvestri <pascualsilvestri14@gmail.com>
2022-11-28 10:12:14 -0300

    barra de tarea casi lista

---
+++ blog/static/js/main.js
@@ -0,0 +1,13 @@
+const titulo = document.title;
+
+const home = document.querySelector('.nav_home');
+
+const title = 'Asociacion Juntos en accion'
+
+if (title != titulo){
+    home.style.display = 'flex'
+}else{
+    home.style.display = 'none'
+}
+
+console.log(titulo)
