e13956d2ce494b4296258512c434c54a08bd77aa
pascual_silvestri <pascualsilvestri14@gmail.com>
2022-11-29 21:33:12 -0300

    modificacion de la barra navegacion

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -3,10 +3,11 @@ const titulo = document.title;
 const home = document.querySelector('.nav_home');
 const title = 'Asociacion Juntos en accion'
 
+
 if (title != titulo){
     home.style.display = 'flex'
 }else{
     home.style.display = 'none'
 }
 // fin del bloque 
-
+    
