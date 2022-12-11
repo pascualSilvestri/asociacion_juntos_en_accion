bd2d3b1ab710fdae5665a282164fa625299e04fa
pascual_silvestri <pascualsilvestri14@gmail.com>
2022-12-11 09:42:25 -0300

    fondo de las ultimas noticas listo

--- blog/static/js/ultimasNoticias.js
+++ blog/static/js/ultimasNoticias.js
@@ -18,28 +18,6 @@ function translado(transladar,arr){
     }
 }
 
-btn_left.addEventListener("click",()=>{
-
-    if (ban0 && !ban100 && !ban_100){
-        translado(tran100,noticia)
-        ban0=false
-        ban100 = true;
-        ban_100 = false
-    }
-
-   
-
-});
-
-btn_rigth.addEventListener("click",()=>{
-
-    if(!ban0 && ban100 && !ban_100){
-        translado(tran0,noticia)
-        ban0=true
-        ban100 = false;
-        ban_100 = false
-    }
-});
 
 
 
