a59b4e550125ec5dbd473c2968e4885d0460f0c4
pascual_silvestri <pascualsilvestri14@gmail.com>
2022-12-12 13:10:18 -0300

    commit rama

--- blog/apps/noticias/views.py
+++ blog/apps/noticias/views.py
@@ -1,3 +1,25 @@
+from datetime import timezone
 from django.shortcuts import render
-
+from django.views.generic import CreateView, DetailView, ListView
 # Create your views here.
+from .models import Noticia
+
+
+
+def ultimasNoti(request):
+    tama = Noticia.objects.all().__len__
+    noticia = Noticia.objects.filter(id__in = [1,2,3]).order_by()
+    context ={'noticia':noticia}
+    
+    return render(request,'noticias.html',context)
+
+
+
+class Mostrar(ListView):
+    model = Noticia
+    template_name = 'noticias.html'
+    
+    def get_contador(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
