5b896592a184ebeba753064c1896b6401b93ce27
pascual_silvestri <pascualsilvestri14@gmail.com>
2022-12-12 14:21:59 -0300

    ultimas noticias listo

--- blog/apps/noticias/views.py
+++ blog/apps/noticias/views.py
@@ -7,19 +7,17 @@ from .models import Noticia
 
 
 def ultimasNoti(request):
-    tama = Noticia.objects.all().__len__
-    noticia = Noticia.objects.filter(id__in = [1,2,3]).order_by()
+    noticia = Noticia.objects.order_by('id').reverse()[:3]
     context ={'noticia':noticia}
-    
     return render(request,'noticias.html',context)
 
 
 
-class Mostrar(ListView):
-    model = Noticia
-    template_name = 'noticias.html'
+# class Mostrar(ListView):
+#     model = Noticia
+#     template_name = 'noticias.html'
     
-    def get_contador(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['now'] = timezone.now()
-        return context
+#     def get_contador(self,**kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['now'] = timezone.now()
+#         return context
