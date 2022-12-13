ef3c52ede6a1ef633fecce7f96b8ea957c934bf8
pascual_silvestri <pascualsilvestri14@gmail.com>
2022-12-13 19:54:51 -0300

    modificacion de filtrado

--- blog/apps/noticias/models.py
+++ blog/apps/noticias/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class Comentario(models.Model):
+    
+    pass
+
 class Categoria(models.Model):
     
     nombre = models.CharField(max_length = 60)
--- blog/apps/noticias/urls.py
+++ blog/apps/noticias/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('', views.ultimasNoti, name = 'ultima'),
     # path('', views.Mostrar.as_view(), name = 'noticias'),
     path('Detalles/<int:pk>', views.detalle_noticia, name = 'detalle'),
+    path('Categoria/<int:pk>', views.filtro_categoria, name = 'filtro'),
 	
 ]
