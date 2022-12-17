58a0f048abd5e884b5472a90261909ff8aac2e3a
javicod92 <javi.javier.jc@gmail.com>
2022-12-17 17:02:39 -0300

    cambios importantes
    cambios

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -6,67 +6,68 @@ const recursos = document.querySelector('.recursos');
 const noticias = document.querySelector('.noticias');
 const contacto = document.querySelector('.contacto');
 const historia = document.querySelector('.historia');
-const btnArriva = document.querySelector(".up")
+const btnArriva = document.querySelector(".up");
 const menu = document.querySelector('.contenedor_nav');
-const btnMenu = document.querySelector('.menu')
-const bienvenido = document.querySelector('.bienvenido')
-const dropdown_content = document.querySelector('.dropdown-content')
-const dropdown = document.querySelector('.dropdown')
+const btnMenu = document.querySelector('.menu');
+const bienvenido = document.querySelector('.bienvenido');
+const dropdown_content = document.querySelector('.dropdown-content');
+const dropdown = document.querySelector('.dropdown');
 
 
-const title = 'Asociacion Juntos en accion |'
-const tituloNoticia = 'Asociacion Juntos en accion | Noticias'
-const tituloHistoria = 'Asociacion Juntos en accion | Historia'
-const tituloRegistrar = 'Asociacion Juntos en accion | Registrar'
-const tituloLogin = 'Asociacion Juntos en accion | Login'
+const title = 'Asociacion Juntos en accion |';
+const tituloNoticia = 'Asociacion Juntos en accion | Noticias';
+const tituloHistoria = 'Asociacion Juntos en accion | Historia';
+const tituloRegistrar = 'Asociacion Juntos en accion | Registrar';
+const tituloLogin = 'Asociacion Juntos en accion | Login';
 
-btnArriva.style.display = "none"
-console.log(titulo)
-console.log(tituloNoticia)
+btnArriva.style.display = "none";
+console.log(titulo);
+console.log(tituloNoticia);
 
 if (title != titulo){
-    home.style.display = 'flex'
-    mision.style.display = 'none'
-    bienvenido.style.display = 'none'
+    home.style.display = 'flex';
+    mision.style.display = 'none';
+    // bienvenido.style.display = 'none';
 }else{
-    home.style.display = 'none'
-    mision.style.display = 'flex'
+    home.style.display = 'none';
+    mision.style.display = 'flex';
 }
 
 if(tituloNoticia == titulo){
-    noticias.style.display = 'none'
+    noticias.style.display = 'none';
     
 }else{
-    noticias.style.display = 'flex'
+    noticias.style.display = 'flex';
 }
 
 if(tituloHistoria == titulo){
-    historia.style.display = 'none'
+    historia.style.display = 'none';
     
 }else{
-    historia.style.display = 'flex'
+    historia.style.display = 'flex';
 }
 
-
-
+console.log("hola");
 
 // fin del bloque 
-dropdown.addEventListener('click',()=>{
-    const img = document.querySelector('.img-drop');
-    dropdown.classList.toggle("dropdown-active")
-    dropdown_content.classList.toggle('activar_select')
-    img.classList.toggle('img-none')
-});
-
+// dropdown.addEventListener('click',()=>{
+//     const img = document.querySelector('.img-drop');
+//     dropdown.classList.toggle("dropdown-active")
+//     dropdown_content.classList.toggle('activar_select')
+//     img.classList.toggle('img-none')
+// });
 
 
 document.addEventListener("scroll", e => {
-    let scrolly = e.path[1].window.scrollY
+    console.log("hola");
+    let scrolly = e.path[1].window.scrollY;
     if (scrolly >= 200) {
-        btnArriva.style.display = "flex"
+        console.log("Llegó a 200");
+        btnArriva.style.display = "flex";
     }
     if (scrolly < 200) {
-        btnArriva.style.display = "none"
+        console.log("No llegó a 200");
+        btnArriva.style.display = "none";
     }
 
 });
